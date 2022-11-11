a93a3a5cfeeab2db71978349a6be9c9dcd33e449
kjh910 <god12nom@gmail.com>
2022-11-11 23:53:14 +0900

    logout

--- airbnb-clone-frontend/app/src/api.tsx
+++ airbnb-clone-frontend/app/src/api.tsx
@@ -1,3 +1,4 @@
+import Cookie from "js-cookie";
 import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -17,5 +18,11 @@ export const getRoom = ({ queryKey }: QueryFunctionContext) => {
 export const getMe = () =>
   instance.get(`users/me`).then((response) => response.data);
 
-export const logOut = () =>
-  instance.post(`users/log-out`).then((response) => response.data);
+  export const logOut = () =>
+    instance
+      .post(`users/log-out`, null, {
+        headers: {
+          "X-CSRFToken": Cookie.get("csrftoken") || "",
+        },
+      })
+      .then((response) => response.data);
--- airbnb-clone-frontend/app/src/components/organisms/header/header.tsx
+++ airbnb-clone-frontend/app/src/components/organisms/header/header.tsx
@@ -1,6 +1,9 @@
-import { Box, Flex, HStack, Stack, useColorModeValue, useDisclosure, useToast } from "@chakra-ui/react";
+import { Box, Flex, Stack, useColorModeValue, useDisclosure, useToast } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import useUser from "../../../lib/useUser";
+import useUser from "../../../libs/useUser";
+
+import { useQueryClient } from "@tanstack/react-query";
+
 import DarkModeIconButton from "../../atoms/buttons/dark-mode-icon-button/dark-mode-icon-button";
 import LoginButton from "../../atoms/buttons/login-button/login-button";
 import SignUpButton from "../../atoms/buttons/sign-up-button/sign-up-button";
@@ -8,6 +11,7 @@ import AirbnbLogos from "../../atoms/logos/airbnb-logos/airbnb-logos";
 import UserMenu from "../../molecules/user-menu/user-menu";
 import LoginModal from "../modal/login-modal/login-modal";
 import SignUpModal from "../modal/sign-up-modal/sign-up-modal";
+import { logOut } from "../../../api";
 
 
 export default function Header() {
@@ -24,22 +28,24 @@ export default function Header() {
       } = useDisclosure();
     const logoColor = useColorModeValue("red.500", "red.200");
     const toast = useToast();
+    const queryClient = useQueryClient();
+
     const onLogOut = async () => {
         const toastId = toast({
-        title: "Login out...",
-        description: "Sad to see you go...",
-        status: "loading",
-        position: "bottom-right",
+            title: "Login out...",
+            description: "Sad to see you go...",
+            status: "loading",
+            position: "bottom-right",
         });
-        /* const data = await logOut();
-        console.log(data); */
-        setTimeout(() => {
+        await logOut();
+
+        queryClient.refetchQueries(["me"]);
+
         toast.update(toastId, {
             status: "success",
             title: "Done!",
             description: "See you later!",
         });
-        }, 5000);
     };
 
     return (
